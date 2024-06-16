882527bc619be88912cabf5063f2a874c5f04bb8
JohnPaulIII <69158887+JohnPaulIII@users.noreply.github.com>
2024-06-16 01:57:30 -0400

    Limelight V1.1.2
    WIP: set control angle for following

--- src/main/java/frc/robot/autonomous_commands/LimelightFollow.java
+++ src/main/java/frc/robot/autonomous_commands/LimelightFollow.java
@@ -25,9 +25,16 @@ public class LimelightFollow extends TeleopMoveCommand{
         double l_x = position[0];
         double l_z = position[2];
         double l_yaw = position[4];
+        double r_yaw = drive.GetGyro();
+
+        double r1 = Math.sqrt(l_x * l_x + l_z * l_z);
+        double r2 = Math.sqrt(l_x * l_x + Math.pow(followDistance - l_z, 2));
+
+        double a1 = Math.acos((r1 * r1 + r2 * r2 - followDistance * followDistance) / (2 * r1 * r2)) * 180 / Math.PI;
+        double angle = r_yaw + l_yaw + a1 - 180;
 
         // left-x, right+x, up-y, down+y
-        return VectorR.fromCartesian(x, y);
+        return VectorR.fromPolar(0.5, angle);
 
     }
 

83531c1f6db2e7329906631806fa9f8513b1ae61
InfernoJager <eharrisonnc329@gmail.com>
2024-10-10 16:36:50 -0400

    Auto V1.0
    Made basic 4 note test auto and auto picker

---
+++ src/main/java/frc/robot/commands/CompositeCommands/AutoIntake.java
@@ -0,0 +1,36 @@
+package frc.robot.commands.CompositeCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.PivotSubsystem;
+import frc.robot.subsystems.ShootSubsystem;
+import frc.robot.subsystems.BeltSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+
+import frc.robot.commands.IntakeCommands.IntakeCommand;
+import frc.robot.commands.BeltCommands.BeltPushCommand;
+import frc.robot.commands.BeltCommands.NoteDetectionCommand;
+import frc.robot.commands.CompositeCommands.NotePrepCommand;
+import frc.robot.commands.PivotCommands.PivotToTargetPIDCommand;
+
+public class AutoIntake extends SequentialCommandGroup {
+    
+    public AutoIntake(PivotSubsystem pivot, BeltSubsystem belt, IntakeSubsystem intake, ShootSubsystem shoot) {
+
+        double target = 21;
+        double safeAngle = 33;
+        double pivotSpeed = 0.3;
+        double intakeSpeed = 0.6;
+        double beltSpeed = intakeSpeed/1.592;
+
+        addCommands(
+            new PivotToTargetPIDCommand(pivot, target, pivotSpeed, 1),
+            new IntakeCommand(intake, intakeSpeed).alongWith(new BeltPushCommand(belt, beltSpeed)).raceWith(new NoteDetectionCommand(belt), new WaitCommand(3)),
+            new NotePrepCommand(belt, shoot).alongWith(new PivotToTargetPIDCommand(pivot, safeAngle, pivotSpeed, 1))
+        );
+
+        addRequirements(pivot, belt, intake);
+
+    }
+
+}
---
+++ src/main/java/frc/robot/commands/DriveCommands/AutoMoveCommand.java
@@ -0,0 +1,77 @@
+package frc.robot.commands.DriveCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.utils.VectorR;
+
+import frc.robot.subsystems.DriveSubsystem;
+
+public class AutoMoveCommand extends Command {
+    
+    final DriveSubsystem drive;
+    private VectorR moveSpeed;
+    private double position;
+    private double desiredPosition; 
+    private double startPosition;
+    private double angle;
+    private double magnitude;
+    private double turnSpeed;
+    // private boolean turning;
+    private double desiredAngle;
+
+
+    public AutoMoveCommand(DriveSubsystem m_drive, double m_distance, double m_angle, double m_magnitude, double m_turnSpeed, double m_desiredAngle) {
+
+        drive = m_drive;
+
+
+        addRequirements(drive);
+
+    }
+
+    @Override
+    public void initialize() {
+        moveSpeed.setFromPolar(magnitude, angle);
+        startPosition = drivePosition();
+    }
+
+    @Override
+    public void execute() {
+
+        if (Math.abs(drive.GetGyro() - desiredAngle) < 1) {
+            turnSpeed = 0;
+        }
+
+        // if (Math.abs(turnSpeed) > 0) {
+        //     turning = true;
+        // } else {
+        //     turning = false;
+        // }
+        
+        drive.move(moveSpeed, turnSpeed, false, false);
+
+        // One revolution is 0.5116 inches
+        // One full rotation is 49.52 revolutions
+        position = Math.abs(drivePosition() - startPosition);
+        // SmartDashboard.putNumber("Expected Position", position);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (position >= desiredPosition) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted){
+        drive.stop();
+    }
+
+    private double drivePosition() {
+        return drive.modules.backRight.driveMotor.getEncoder().getPosition();
+    }
+
+}
